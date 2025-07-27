b9604d8a5d2bbc33e8561567c14ec04612ba5904
chiofus <132926798+chiofus@users.noreply.github.com>
2025-07-27 15:30:50 -0400

    finished ex 07

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,17 @@
-const reverseString = function() {
+/**
+ * 
+ * @param {string} to_reverse 
+ * @returns {string}
+ */
+const reverseString = function(to_reverse) {
+    let reversed = "";
+    let str_length = to_reverse.length;
 
+    for (let i = 0; i < str_length; i++) {
+        reversed += to_reverse[str_length-1-i];
+    }
+
+    return reversed;
 };
 
 // Do not edit below this line
