28e3fa7eaabcb6ebf9c1a104c555776ff24dbf7a
chiofus <132926798+chiofus@users.noreply.github.com>
2025-07-27 15:23:05 -0400

    finished exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(to_repeat, repetitions) {
+    let repeated_string = "";
 
+    if (repetitions < 0) {
+        return "ERROR";
+    }
+
+    for (let i = 0; i < repetitions; i++) {
+        repeated_string += to_repeat;
+    }
+
+    return repeated_string;
 };
 
 // Do not edit below this line
