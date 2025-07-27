eba56007fa6b297db56046c48893512a1141275e
chiofus <132926798+chiofus@users.noreply.github.com>
2025-07-27 15:49:49 -0400

    finished ex 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+/**
+ * 
+ * @param {Array} og_array 
+ * @param  {...any} to_remove
+ * @returns {Array}
+ */
+const removeFromArray = function(og_array, ...to_remove) {
+    let new_array = structuredClone(og_array);
+
+    for (let i = 0; i < to_remove.length; i++) {
+        while (new_array.includes(to_remove[i])) {
+            new_array.splice(new_array.indexOf(to_remove[i]), 1);
+        }
+    }
+
+    return new_array;
 };
 
 // Do not edit below this line
