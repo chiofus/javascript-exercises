48ec11ecf297afd7d63558fe8e8b4113cde92a09
chiofus <132926798+chiofus@users.noreply.github.com>
2025-07-27 22:56:35 -0400

    ex 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,10 @@
-const leapYears = function() {
-
+/**
+ * 
+ * @param {Number} year 
+ * @returns {boolean}
+ */
+const leapYears = function(year) {
+    return (year % 4 === 0 && year % 100 !== 0 || (year % 4 === 0 && year % 400 === 0))
 };
 
 // Do not edit below this line
