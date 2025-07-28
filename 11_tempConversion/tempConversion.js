c77159519d6b075b4f2ba4fd16a3bf07ec47aefe
chiofus <132926798+chiofus@users.noreply.github.com>
2025-07-27 23:14:54 -0400

    ex 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+/**
+ * 
+ * @param {number} farenheit
+ * 
+ * @returns {number}
+ */
+const convertToCelsius = function(farenheit) {
+  return Math.round((farenheit - 32) * 5/9 * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+
+/**
+ * 
+ * @param {number} celsius
+ * 
+ * @returns {number}
+ */
+const convertToFahrenheit = function(celsius) {
+  return Math.round((celsius * 9/5 + 32) * 10)/10;
 };
 
 // Do not edit below this line
