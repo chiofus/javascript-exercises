696dbf83807ede54e7357dac2c0b514fd2427396
chiofus <132926798+chiofus@users.noreply.github.com>
2025-08-11 20:58:29 -0400

    calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+/**
+ * 
+ * @param  {...number} numbers 
+ */
+const add = function(...numbers) {
+	return numbers.reduce((addition, curr) => addition + curr);
 };
 
-const subtract = function() {
-	
+/**
+ * 
+ * @param  {...number} numbers 
+ */
+const subtract = function(...numbers) {
+	return numbers.reduce((subtraction, curr) => subtraction - curr);
 };
 
-const sum = function() {
-	
+/**
+ * @param {number[]} to_sum
+ */
+const sum = function(to_sum) {
+	return to_sum.reduce((curr_sum, a) => curr_sum + a, 0);
 };
 
-const multiply = function() {
-
+/**
+ * @param {number[]} to_mult
+ */
+const multiply = function(to_mult) {
+  return to_mult.reduce((mult, a) => mult * a);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	if (a === 1 || a === 0) {return 1};
+
+  let factorial = a;
+  while (a !== 1) {
+    factorial = factorial * (a-1);
+    a --;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
