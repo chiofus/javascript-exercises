9c3ac11c1d97da36d28e7ab2f0f36b6ed29931d9
chiofus <132926798+chiofus@users.noreply.github.com>
2025-08-11 21:22:42 -0400

    palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+/**
+ * 
+ * @param {string} str
+ * @returns {boolean}
+ */
+const palindromes = function (str) {
+    const to_remove = [' ', '.', ',', ';', ':', '!', '?'];
 
+    let chars = str.split('').filter(chr => !(to_remove.includes(chr))).map(chr => chr.toLowerCase())
+
+    for (let i = 0; i < chars.length/2; i++) {
+        if (chars[i] !== chars[(chars.length-i-1)]) {return false;}
+    }
+
+    return true;
 };
 
 // Do not edit below this line
