3c7243d2e7e0576fbdedb9b28f6a8fe073fbfd98
chiofus <132926798+chiofus@users.noreply.github.com>
2025-08-11 21:30:08 -0400

    fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+/**
+ * 
+ * @param {number} nbr_at
+ * @returns {number}
+ */
+const fibonacci = function(nbr_at) {
+    let seq = [1,1];
 
+    try {
+        nbr_at = parseInt(nbr_at);
+        if (nbr_at < 0) {throw "NEGATIVE" };
+    }
+    catch {
+        return "OOPS";
+    }   
+
+    if (nbr_at === 0) {return 0};
+
+    while (seq.length < nbr_at) {
+        seq.push(seq[seq.length-1] + seq[seq.length-2]);
+    }
+
+    return seq[nbr_at-1];
 };
 
 // Do not edit below this line
